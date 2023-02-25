7704be15d6c76f33512d6bd9e7fd6c36cfb76660
Olena Zheliezova <lena.zhelezova@gmail.com>
2023-02-24 23:20:32 -0500

    Build pipeline config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+user = os.environ.get('MYSQL_USER')
+password = os.environ.get('MYSQL_PASSWORD')
+server = os.environ.get('MYSQL_SERVER')
+database = os.environ.get('MYSQL_DATABASE')
+
+class Config():
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{server}/{database}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
