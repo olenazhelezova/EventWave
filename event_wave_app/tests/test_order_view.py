cc5a8f71c91fec56e5ec46b78ad3e044e03b27ff
Olena Zheliezova <lena.zhelezova@gmail.com>
2023-03-12 16:28:18 -0400

    Fix for order view test

--- event_wave_app/tests/test_order_view.py
+++ event_wave_app/tests/test_order_view.py
@@ -1,11 +1,16 @@
 import unittest
 from event_wave_app import app
 
+
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_orders_route(self):
-        response = self.app.get('/orders')
+    @unittest.mock.patch(
+        "event_wave_app.service.customer_service.CustomerService.get_customers"
+    )
+    def test_orders_route(self, get_customers_mock):
+        get_customers_mock.return_value = []
+        response = self.app.get("/orders")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Ticket orders', response.data)
+        self.assertIn(b"Ticket orders", response.data)
